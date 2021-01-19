9ebaf679404d664f6bb9dab73b07dad80312c03a
tianx <xueqing.tian@agilysys.com>
2021-01-19 16:38:06 -0500

    Components 8

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    render() { 
-        return (<div><h1>Hello Wolrd</h1><button>Increment</button></div>);
+    state = {
+        count: 0,
+    };
+    
+    render() {
+        return (
+            <div>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
- 
+
 export default Counter;
