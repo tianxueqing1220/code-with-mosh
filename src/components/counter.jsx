fe73374c80887919799fa02d2eda28e519d2ad20
tianx <xueqing.tian@agilysys.com>
2021-01-19 15:23:44 -0500

    initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return (<div><h1>Hello Wolrd</h1><button>Increment</button></div>);
+    }
+}
+ 
+export default Counter;
